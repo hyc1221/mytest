21428f2d1c04c3cb211aeb1c851436769c5e3d43
hyc <egorgoodor@mail.ru>
2022-06-16 23:47:33 +0300

    fix model

--- mytest/social/models.py
+++ mytest/social/models.py
@@ -5,20 +5,10 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class UserProfile(models.Model):
 
-    class Genders(models.TextChoices):
-        MALE = 'M', _('Male')
-        FEMALE = 'F', _('Female') 
-    
-    class Status(models.TextChoices):
-        ONLINE = 'ON', _('Online')
-        OFFLINE = 'OFF', _('Offline')
     user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE, related_name="profile")
-    first_name = models.CharField("First name", max_length=255)
-    last_name = models.CharField("Last name", max_length=255)
-    birth_date = models.DateField("Birth date")
-    gender = models.CharField("Gender", max_length=1, choices=Genders.choices)
+    first_name = models.CharField("First name", max_length=255, blank=True, null=True)
+    last_name = models.CharField("Last name", max_length=255, blank=True, null=True)
     description = models.TextField('Description', blank=True, null=True)
-    status = models.CharField("Status", max_length=3, choices=Status.choices, default=Status.OFFLINE)
     last_time = models.DateTimeField("Last time", auto_now_add=True)
     created_at=models.DateTimeField("Created at", auto_now_add=True)
 
